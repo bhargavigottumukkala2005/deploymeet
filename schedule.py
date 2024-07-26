e00aaf2bfd6371da1f5499f806a9c77d5520092a
bhargavigottumukkala2005 <154610392+bhargavigottumukkala2005@users.noreply.github.com>
2024-07-26 18:10:55 +0530

    schedule.py

--- schedule.py
+++ schedule.py
@@ -148,4 +148,4 @@ def refresh_token():
         return "No refresh token found."
 
 if __name__ == "__main__":
-    app.run(debug=True, port=3000)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 3000)))
